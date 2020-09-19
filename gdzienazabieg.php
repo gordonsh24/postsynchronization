42f7e112b1dbfc593f0d66f674d82ffa4468f6c2
Jakub Bis <jakub.b@onthegosystems.com>
2020-09-19 18:03:22 +0800

    Initialize composer

---
+++ gdzienazabieg.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package GdzieNaZabieg
+ */
+/*
+Plugin Name: GdzieNaZabieg
+Description: It provides set of custom functionalities used by gdzienazabieg.com website
+Version: 0.0.1
+Author: Jakub Bis
+Text Domain: gdzienazabieg
+*/
+
+
+
