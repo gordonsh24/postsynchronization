4ba172c97cd3478fdf66af339b5ca8020d374547
Jakub Bis <jakub.b@onthegosystems.com>
2020-09-20 10:22:06 +0800

    Skeleton for post synchronization

---
+++ classes/PostSynchronization/Actions.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace GdzieNaZabieg\PostSynchronization;
+
+
+use WPML\FP\Curryable;
+use WPML\FP\Either;
+
+/**
+ * Class Actions
+ * @package GdzieNaZabieg\PostSynchronization
+ *
+ * @method static callable|Either create( ...$siteData, ...$post ): Curried :: SiteData->\WP_Post->Either
+ *
+ * @method static callable|Either update( ...$siteData, ...$post ): Curried :: SiteData->\WP_Post->Either
+ */
+class Actions {
+	use Curryable;
+
+	public static function init() {
+
+		self::curryN( 'create', 2, function ( SiteData $siteData, \WP_Post $post ) {
+			$response = wp_remote_post( $siteData->url . '/wp-json/wp/v2/posts', [
+				'headers' => [
+					'Authorization' => 'Basic ' . base64_encode( $siteData->user . ':' . $siteData->password )
+				],
+				'body'    => [
+					'title'      => $post->post_title,
+					'status'     => $post->post_status,
+					'content'    => $post->post_content,
+					'categories' => 3,
+					'excerpt'    => $post->post_excerpt,
+				]
+			] );
+
+			return is_wp_error( $response ) ? Either::left( $response ) : Either::right( $response );
+		} );
+
+		self::curryN( 'update', 2, function ( SiteData $siteData, \WP_Post $post ) {
+			$response = wp_remote_post( $siteData->url . '/wp-json/wp/v2/posts/' . $post->ID, [
+				'headers' => [
+					'Authorization' => 'Basic ' . base64_encode( $siteData->user . ':' . $siteData->password )
+				],
+				'body'    => [
+					'title'      => $post->post_title,
+					'status'     => $post->post_status,
+					'content'    => $post->post_content,
+					'categories' => 3,
+					'excerpt'    => $post->post_excerpt,
+				]
+			] );
+
+			return is_wp_error( $response ) ? Either::left( $response ) : Either::right( $response );
+		} );
+	}
+
+}
+
+Actions::init();
--- gdzienazabieg.php
+++ gdzienazabieg.php
@@ -10,5 +10,57 @@ Author: Jakub Bis
 Text Domain: gdzienazabieg
 */
 
+require_once 'vendor/autoload.php';
+
+$siteData           = new \GdzieNaZabieg\PostSynchronization\SiteData();
+$siteData->url      = 'http://gdzienazabieg.test/';
+$siteData->user     = 'admin';
+$siteData->password = 'password';
+
+add_action( 'save_post_post', function ( $postId, \WP_Post $post, $update ) use ( $siteData ) {
+
+	$action = $update ?
+		\GdzieNaZabieg\PostSynchronization\Actions::update( $siteData ) :
+		\GdzieNaZabieg\PostSynchronization\Actions::create( $siteData );
+
+	/** @var \WPML\FP\Either $result */
+	$result = $action( $post );
+
+	if ( \WPML\FP\Fns::isLeft( $result ) ) {
+		error_log( $result->orElse( \WPML\FP\Fns::identity() )->get() );
+	}
+
+}, 10, 3 );
 
 
+if ( isset( $_GET['jakub'] ) ) {
+
+	$create = \GdzieNaZabieg\PostSynchronization\Actions::create( $siteData );
+
+//	$api_response = wp_remote_post( 'http://gdzienazabieg.test/wp-json/wp/v2/posts', [
+//		'headers' => [
+//			'Authorization' => 'Basic ' . base64_encode( 'admin:password' )
+//		],
+//		'body'    => [
+//			'title'      => 'My test',
+//			'status'     => 'draft', // ok, we do not want to publish it immediately
+//			'content'    => 'lalala',
+//			'categories' => 3, // category ID
+//			'excerpt'    => 'Read this awesome post',
+//			'slug'       => 'new-test-post',
+//		]
+//	] );
+
+//	$post = new \WP_Post()
+//
+//	$api_response = $create()
+
+	$body = json_decode( $api_response['body'] );
+
+	print_r( $body ); // or print_r( $api_response );
+
+	if ( wp_remote_retrieve_response_message( $api_response ) === 'Created' ) {
+		echo 'The post ' . $body->title->rendered . ' has been created successfully';
+	}
+	die;
+}
